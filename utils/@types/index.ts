52b1b18839837f4e2e83f50874d4c862447c1407
seyithantopal <seyithan_41_@hotmail.com>
2022-12-30 11:25:16 +0100

    chore: install axios

--- utils/@types/index.ts
+++ utils/@types/index.ts
@@ -1,3 +1,17 @@
 export type SwipeableType = {
   content: React.ReactNode;
 };
+
+export interface Doctor {
+  name: string;
+  slug: string;
+  city: string;
+  country: string;
+  qunoScoreNumber: number;
+  qunoScoreText: 'Excelent' | 'Very Good' | 'Good' | 'Regular' | 'Bad';
+  ratingsAverage: number;
+  treatmentsLastYear: number;
+  yearsExperience: number;
+  basePrice: number;
+  avatarUrl: string;
+}
